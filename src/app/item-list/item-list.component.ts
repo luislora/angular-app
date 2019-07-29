9ac5fc6fd9a433fae8832e234cf2ae2c0a96184c
Luis Lora <luisgabriellora@gmail.com>
2019-07-29 16:10:27 -0500

    Added create item functionality

--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from '../types';
+import { ItemService } from '../item.service';
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-item-list',
@@ -8,24 +10,29 @@ import { Item } from '../types';
 })
 export class ItemListComponent implements OnInit {
 
-  itemList: Array<Item> = [
-    {
-      id: 1,
-      title: 'First item'
-    },
-    {
-      id: 2,
-      title: 'Second item'
-    },
-    {
-      id: 3,
-      title: 'Third item'
-    }
-  ];
+  itemList: Array<Item>;
+  itemForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private itemService: ItemService,
+    formBuilder: FormBuilder
+  ) {
+    this.itemForm = formBuilder.group({
+      itemTitle: ''
+    });
+  }
 
   ngOnInit() {
+    this.itemList = this.itemService.getItems();
+  }
+
+  removeItem(id) {
+    this.itemService.removeItem(id);
+  }
+
+  processChange(formValue) {
+    this.itemService.addItem(formValue.itemTitle);
+    this.itemForm.reset();
   }
 
 }
---
+++ src/app/item.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { Item } from './types';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+
+  itemList: Array<Item>;
+
+  constructor() {
+    this.itemList = [
+      {
+        id: 1,
+        title: 'First item'
+      },
+      {
+        id: 2,
+        title: 'Second item'
+      },
+      {
+        id: 3,
+        title: 'Third item'
+      }
+    ];
+  }
+
+  getItems() {
+    return this.itemList;
+  }
+
+  removeItem(id) {
+    const index = this.itemList.indexOf(id);
+    
+    this.itemList.splice(index, 1);
+  }
+
+  addItem(newTitle) {
+    const nextIndex = this.itemList.length + 1;
+    
+    this.itemList.push(
+      {
+        id: nextIndex,
+        title: newTitle
+      }
+    );
+    
+  }
+}
